4378884bf67ee5d7f63f960fa76940a3ae1ed465
yukimaru32 <cyuuki15@gmail.com>
2025-02-07 19:31:12 -0700

    added dark mode function in my page

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -5,3 +5,30 @@ menubutton.addEventListener("click",() =>{
     mainnav.classList.toggle("show");
     menubutton.classList.toggle("show");
 });
+
+const modeButton = document.querySelector("input");
+const title = document.querySelector(".titletop");
+const navigation = document.querySelector("ul");
+const main = document.querySelector("#main");
+const button = document.getElementById("button");
+const display = document.getElementById("display");
+const footer = document.querySelector("footer");
+
+modeButton.addEventListener("click", ()=>{
+    if(display.textContent.includes("normal")){
+        title.style.background ="var(--dark)"
+        navigation.style.background ="var(--blue)"
+        main.style.background ="var(--black)"
+        button.style.background ="var(--blue)"
+        footer.style.background ="var(--dark)"
+        display.textContent ="dark"
+    } else{
+        title.style.background ="var(--blue)"
+        navigation.style.background = "var(--dark)"
+        main.style.background ="var(--white)"
+        button.style.background ="var(--red)"
+        footer.style.background ="var(--blue)"
+        display.textContent ="normal"
+    }
+    
+});
