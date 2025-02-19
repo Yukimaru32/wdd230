e85dd7c9863ed3b93c09aa128883f4f9d0e366de
yukimaru32 <cyuuki15@gmail.com>
2025-02-19 13:41:34 -0700

    added some js function

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,5 +1,5 @@
 const temperature = 10;//in celcius
-const windSpeed = 7;//km/h
+const windSpeed = 5;//km/h
 
 function calculateWindChill(temp, speed){
     return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(2);
@@ -11,9 +11,13 @@ function displayWindChill(){
         // Calculate and display wind chill
         const windChill = calculateWindChill(temperature, windSpeed);
         document.getElementById("WindChill").textContent = `WindChill: ${windChill} °C`;
+        document.getElementById("temp").textContent =`Temprature: ${temperature} °C`;
+        document.getElementById("wind").textContent=`Wind Speed: ${windSpeed} km/h`;
     } else {
         // Display "N/A" if conditions are not met
         document.getElementById("WindChill").textContent = 'WindChill: N/A';
+        document.getElementById("temp").textContent =`Temprature: ${temperature} °C`;
+        document.getElementById("wind").textContent=`Wind Speed: ${windSpeed} km/h`;
     }
 }
 document.addEventListener('DOMContentLoaded', displayWindChill);
