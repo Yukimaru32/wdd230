e2ba54c4246a9a004d5c922aa95bc6398af78f6b
yukimaru32 <cyuuki15@gmail.com>
2025-02-20 23:57:20 -0700

    updated my homepage

---
+++ scripts/visit.js
@@ -0,0 +1,18 @@
+// 1️⃣ Initialize display element variable
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+
+numVisits++;
+
+
+localStorage.setItem("numVisits-ls", numVisits);
+
