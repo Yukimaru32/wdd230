31fc2c1d72627e479fc82ecfe916695dca195ee9
yukimaru32 <cyuuki15@gmail.com>
2025-04-02 18:59:53 -0600

    added some data

--- japan/scripts/visit.js
+++ japan/scripts/visit.js
@@ -19,3 +19,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     localStorage.setItem("lastVisit", now);
 });
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else if (numVisits ==1000){
+    visitsDisplay.textContent =`You are the 1000th visitor!!! Conguraturations 🥳 🥳 🥳`
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
