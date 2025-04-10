e37606ec95e4398ff4a9039e4bdbe30164d187f6
yukimaru32 <cyuuki15@gmail.com>
2025-04-09 20:19:08 -0600

    changed some

--- final/scripts/pricing.js
+++ final/scripts/pricing.js
@@ -1,19 +1,25 @@
-function loadPricing() {
-    fetch('data/pricing.json')
-        .then(response => response.json())
-        .then(data => {
-            let tableBody = "";
-            data.rentals.forEach(rental => {
-                tableBody += `<tr>
-                    <td>${rental.name}</td>
-                    <td>${rental.max_persons}</td>
-                    <td>$${rental.prices.reservation.half_day}</td>
-                    <td>$${rental.prices.reservation.full_day}</td>
-                    <td>$${rental.prices.walk_in.half_day}</td>
-                    <td>$${rental.prices.walk_in.full_day}</td>
-                </tr>`;
-            });
-            document.querySelector('#pricing-table tbody').innerHTML = tableBody;
-        })
-        .catch(err => console.error("Error loading pricing data:", err));
+const directoryURL ="https://yukimaru32.github.io/wdd230/final/data/pricing.json";
+
+async function getLinks() {
+    const response = await fetch(directoryURL);
+    const data = await response.json();
+    displayLinks(data.rentals); // Now passing the lessons array
+}
+getLinks();
+const displayLinks = (rentals) =>{
+    const container = document.getElementById("pricing-table");
+
+    rentals.forEach(rental => {
+        tableBody += `<tr>
+            <td>${rental.name}</td>
+            <td>${rental.max_persons}</td>
+            <td>$${rental.prices.reservation.half_day}</td>
+            <td>$${rental.prices.reservation.full_day}</td>
+            <td>$${rental.prices.walk_in.half_day}</td>
+            <td>$${rental.prices.walk_in.full_day}</td>
+        </tr>`;
+
+        container.appendChild(tableBody);
+    });
+
 }
